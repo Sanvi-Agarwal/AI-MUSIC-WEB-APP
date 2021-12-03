a57ecc4804f733b67120d8afd8f1f6781ffa1e6f
Sanvi-Agarwal <85011956+Sanvi-Agarwal@users.noreply.github.com>
2021-12-03 13:18:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload() {
     song1 = loadSound("music1.mp3");
@@ -12,6 +16,25 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWrist=" + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        consle.log("rightWristX=" + rightWristX + "rightWristY=" + rightWristY);
+    }
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
 }
 
 function draw() {
