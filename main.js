25002865d22cb0b942161b55427cf373b4d14821
Sanvi-Agarwal <85011956+Sanvi-Agarwal@users.noreply.github.com>
2021-12-04 15:30:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
+song1_status = "";
+song2_status = "";
 
 function preload() {
     song1 = loadSound("music1.mp3");
@@ -29,7 +33,10 @@ function gotPoses(results) {
 
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
-        consle.log("rightWristX=" + rightWristX + "rightWristY=" + rightWristY);
+        console.log("rightWristX=" + rightWristX + "rightWristY=" + rightWristY);
+
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
     }
 }
 
@@ -39,4 +46,34 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0, 600, 500);
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    song1_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
+
+    if (scoreLeftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+
+        song1.stop();
+        if (song1_status == false) {
+            song2.play();
+            document.getElementById("song").innerHTML = "Playing : Ranjha";
+        }
+    }
+    if (scoreRightWrist > 0.2) {
+        circle(rightWristX, rightWristY, 20);
+
+        song2.stop();
+        if (song2_status == false) {
+            song1.play();
+            document.getElementById("song").innerHTML = "Playing : Rattan Lambiya";
+        }
+    }
+}
+
+function play() {
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
